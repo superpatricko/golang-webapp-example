778cc66f8dcbed432eb72ef998244e09735e3faa
Patrick Nguyen <superpatricko@gmail.com>
2015-11-25 14:21:45 -0500

    Add editHandler
    Currently hardcoding the HTML, it's ugly

--- wiki.go
+++ wiki.go
@@ -36,7 +36,28 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+// Loads the pages (or, if it doesn't exist, create an empty Page struct), and displays an HTML form
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+	"<form action=\"/save/%s\" method=\"POST\">"+
+	"<textarea name=\"body\">%s</textarea><br>"+
+	"<input type=\"submit\" value=\"Save\">"+
+	"</form>",
+		p.Title, p.Title, p.Body)
+}
+
+func saveHandler(w http.Response, r *http.Request) {
+
+}
+
 func main() {
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+//	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
